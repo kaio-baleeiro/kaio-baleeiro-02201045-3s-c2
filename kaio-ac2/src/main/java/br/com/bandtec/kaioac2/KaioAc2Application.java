21ff864b431c20f50a4413a620ec3628967adac7
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-04-16 16:53:00 -0300

    Diretório do projeto criado

---
+++ kaio-ac2/src/main/java/br/com/bandtec/kaioac2/KaioAc2Application.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.kaioac2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaioAc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaioAc2Application.class, args);
+	}
+
+}
---
+++ kaio-ac2/src/test/java/br/com/bandtec/kaioac2/KaioAc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.kaioac2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaioAc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
