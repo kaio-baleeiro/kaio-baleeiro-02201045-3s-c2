a871c2efc7294df534ad9fd7269367b91a83c98c
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-04-16 18:29:51 -0300

    Atualização projeto

--- kaio-ac2/src/main/java/br/com/bandtec/kaioac2/dominio/Lutador.java
+++ kaio-ac2/src/main/java/br/com/bandtec/kaioac2/dominio/Lutador.java
@@ -20,7 +20,7 @@ public class Lutador {
     private Double forcaGolpe;
     private Double vida = 100.00;
     private Integer concentracoesRealizadas = 0;
-    private boolean vivo = true;
+    private boolean vivo = (vida > 0) ? true : false;
 
     public Integer getId() {
         return id;
